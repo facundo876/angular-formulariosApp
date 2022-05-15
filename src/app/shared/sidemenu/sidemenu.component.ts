d92da61b7b0acf440e9194c26d91a150f4ee7f77
Facundo Arce <facundo.ea07@gmail.com>
2022-05-15 02:32:27 -0300

    creacion modulos, componenetes y formulario basicos

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string,
+  ruta: string
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+  ]
+})
+export class SidemenuComponent {
+
+  templateMenu: MenuItem [] = [
+    {
+      texto: 'Básicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Básicos',
+      ruta: './template/switches'
+    }
+  ];
+
+  reactiveMenu: MenuItem [] = [
+    {
+      texto: 'Básicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Básicos',
+      ruta: './reactive/switches'
+    }
+  ];
+
+}
